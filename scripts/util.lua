8d97cdf891d738c2a40103f62049fa3636eecc29
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2024-10-22 16:47:41 +0200

    Add return hints

--- scripts/util.lua
+++ scripts/util.lua
@@ -1,16 +1,19 @@
 ---@param haystack string
 ---@param needle string
+---@return boolean
 function string.starts(haystack, needle)
 	return string.sub(haystack, 1, string.len(needle)) == needle
 end
 
 ---@param haystack string
 ---@param needle string
+---@return boolean
 function string.ends(haystack, needle)
 	return string.sub(haystack, string.len(needle) * -1) == needle
 end
 
 ---@param x number
+---@return number
 function math.absfloor(x)
 	if x > 0 then
 		return math.floor(x)
@@ -20,11 +23,13 @@ function math.absfloor(x)
 end
 
 ---@param x number
+---@return number
 function math.round(x)
 	return math.floor(x + 0.5)
 end
 
 ---@param table table
+---@return boolean
 function has_value(table, needle)
 	for _, value in ipairs(table) do
         if value == needle then
