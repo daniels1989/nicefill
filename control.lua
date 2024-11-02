cc4fb59b1be3212299d14b5715a96abae4ce3746
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2024-11-02 19:23:27 +0100

    Formatting

--- control.lua
+++ control.lua
@@ -62,84 +62,68 @@ function do_nicefill( surface_index, tiles )
 	surface.set_tiles( nice_tiles )
 end
 
-script.on_event(defines.events.on_robot_built_tile,
-	function(event)
-		if DEBUG then
-			log( "NiceFill on_robot_built_tile" )
-			log( serpent.block( event ) )
-		end
+script.on_event(defines.events.on_player_built_tile, function(event)
+	if DEBUG then
+		log( "NiceFill on_player_built_tile" )
+		log( serpent.block( event ) )
+	end
 
-		local tiles = convert_old_tile_and_position(event.tile, event.tiles)
+	local tiles = convert_old_tile_and_position(event.tile, event.tiles)
 
-		if not pcall(do_nicefill, event.surface_index, tiles ) then
-			log( "NiceFill failed." )
-			debug.print( "NiceFill failed." );
+	if not pcall(do_nicefill, event.surface_index, tiles ) then
+		log( "NiceFill failed." )
+		debug.print( "NiceFill failed." );
+	end
+end)
+
+script.on_event(defines.events.on_robot_built_tile, function(event)
+	if DEBUG then
+		log( "NiceFill on_robot_built_tile" )
+		log( serpent.block( event ) )
+	end
+
+	local tiles = convert_old_tile_and_position(event.tile, event.tiles)
+
+	if not pcall(do_nicefill, event.surface_index, tiles ) then
+		log( "NiceFill failed." )
+		debug.print( "NiceFill failed." );
+	end
+end)
+
+script.on_event(defines.events.script_raised_set_tiles, function(event)
+	if DEBUG then
+		log( "NiceFill script_raised_set_tiles" )
+		log( serpent.block( event ) )
+	end
+
+	if not event.tiles or not event.tiles[1] then return end
+
+	if not pcall(do_nicefill, event.surface_index, event.tiles ) then
+		log( "NiceFill failed." )
+	end
+end)
+
+script.on_event(defines.events.on_surface_created, function(event)
+	local surface = game.get_surface(event.surface_index)
+
+	if surface == nil then
+		log( "NiceFill on_surface_created found no surface." )
+		return
+	end
+
+	if not NiceFill.is_nicefill_surface(surface) then return end
+
+	debug.print('NiceFill created surface ' .. surface.name)
+
+	if remote.interfaces["RSO"] then -- RSO compatibility
+		if pcall(remote.call, "RSO", "ignoreSurface", surface) then
+			if DEBUG then log( "NiceFill surface registered with RSO." ) end
+		else
+			log( "NiceFill surface failed to register with RSO" )
+			debug.print( "NiceFill failed to register surface with RSO" )
 		end
 	end
-)
-
-script.on_event(defines.events.on_player_built_tile,
-	function(event)
-		if DEBUG then
-			log( "NiceFill on_player_built_tile" )
-			log( serpent.block( event ) )
-		end
-
-		local tiles = convert_old_tile_and_position(event.tile, event.tiles)
-
-		if not pcall(do_nicefill, event.surface_index, tiles ) then
-			log( "NiceFill failed." )
-			debug.print( "NiceFill failed." );
-		end
-	end
-)
-
-script.on_event(defines.events.script_raised_set_tiles,
-	function(event)
-		if DEBUG then
-			log( "NiceFill script_raised_set_tiles" )
-			log( serpent.block( event ) )
-		end
-
-		if not event.tiles or not event.tiles[1] then return end
-
-		if not pcall(do_nicefill, event.surface_index, event.tiles ) then
-			log( "NiceFill failed." )
-		end
-	end
-)
-
-script.on_event(defines.events.on_chunk_generated,
-	function(event)
-		if NiceFill.is_nicefill_surface(event.surface) then
-			debug.print(serpent.block( event.area ) )
-		end
-	end
-)
-
-script.on_event(defines.events.on_surface_created,
-	function(event)
-		local surface = game.get_surface(event.surface_index)
-
-		if surface == nil then
-			log( "NiceFill on_surface_created found no surface." )
-			return
-		end
-
-		if not NiceFill.is_nicefill_surface(surface) then return end
-
-		debug.print('NiceFill created surface ' .. surface.name)
-
-		if remote.interfaces["RSO"] then -- RSO compatibility
-			if pcall(remote.call, "RSO", "ignoreSurface", surface) then
-				if DEBUG then log( "NiceFill surface registered with RSO." ) end
-			else
-				log( "NiceFill surface failed to register with RSO" )
-				debug.print( "NiceFill failed to register surface with RSO" )
-			end
-		end
-	end
-)
+end)
 
 -- /c remote.call("NiceFill", "spawn_tiles")
 -- remote.add_interface("NiceFill", {
@@ -171,5 +155,11 @@ if DEBUG then
 
 		local tiles = player.surface.find_tiles_filtered{ position = player.position, radius = 2}
 		for _, tile in pairs(tiles) do debug.print(tile.name) end
+	end)
+
+	script.on_event(defines.events.on_chunk_generated, function(event)
+		if NiceFill.is_nicefill_surface(event.surface) then
+			debug.print(serpent.block( event.area ) )
+		end
 	end)
 end
