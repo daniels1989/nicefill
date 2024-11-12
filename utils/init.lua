4800f750c097bc8fbe3c4125f4866403e2101330
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2024-11-12 14:32:43 +0100

    Fix file loading

--- utils/init.lua
+++ utils/init.lua
@@ -1,6 +1,6 @@
 local Utils = {}
 
-Utils.table = require './table'
-Utils.string = require './string'
+Utils.table = require '__nicefill-scriptfix__/utils/table'
+Utils.string = require '__nicefill-scriptfix__/utils/string'
 
 return Utils
